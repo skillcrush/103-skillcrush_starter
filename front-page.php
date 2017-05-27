802cb7fe9adf77c28048d38f890b4e6bdce00c11
Ann Cascarano <a.cascarano@gmail.com>
2017-05-26 21:18:34 -0400

    skillcrushstarter-08

---
+++ front-page.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for the homepage
+ *
+ * @package WordPress
+ * @subpackage Skillcrush_Starter
+ * @since Skillcrush Starter 2.0
+ */
+
+get_header(); ?>
+
+<section class="home-page">
+	<div class="main-content">
+		<div class="content">
+			<?php while ( have_posts() ): the_post(); ?>
+				<h1><?php the_title(); ?></h1>
+				<?php the_content(); ?>
+				<div class="social-btns">  <!-- populate or remove as many of these as you want -->
+					<a href="" class="soc-icon tw"></a>
+					<a href="" class="soc-icon fb"></a>
+					<a href="" class="soc-icon ln"></a>
+					<a href="" class="soc-icon db"></a>
+					<a href="" class="soc-icon gh"></a>
+					<a href="" class="soc-icon in"></a>
+				</div>
+				<a href="<?php echo site_url('/blog/'); ?>" class="btn">View My Blog</a>
+			<?php endwhile; ?>
+		</div>
+	</div>
+</section>
+
+<?php get_footer(); ?>
